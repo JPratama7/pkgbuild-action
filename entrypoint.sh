03bcfc2d99205ea1fd1b46ab152c3fd8031ce998
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2023-04-19 20:52:40 +0700

    fix error SCRINFO

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,7 @@ sudo -H -u builder updpkgsums
 # Assume that if .SRCINFO is missing or mismatch
 # Recreating .SRCINFO
 echo "Creating .SRCINFO"
-sudo -H -u builder echo $(makepkg --printsrcinfo) > .SRCBUILD
+echo $(sudo -H -u builder makepkg --printsrcinfo) > .SRCBUILD
 
 # Extract AUR dependencies from .SRCINFO (depends or depends_x86_64) and install
 mapfile -t PKGDEPS < \
