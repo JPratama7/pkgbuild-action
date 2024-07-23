fa63032280772244c9e3244979116cfb23bc981a
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2024-07-24 02:52:38 +0700

    add pacman contrib

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ FILE="$(basename "$0")"
 
 echo $FILE
 
-pacman -Syu --noconfirm wget pkgconf cmake ninja meson 
+pacman -Syu --noconfirm wget pkgconf cmake ninja meson pacman-contrib 
 
 #force pod2man
 ln -s /usr/bin/core_perl/pod2man /usr/bin/pod2man
