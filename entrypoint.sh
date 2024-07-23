9f00b68229a7af0aa97effba1d62ef34d0196998
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2024-07-24 06:39:06 +0700

    fix unbound variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,16 +41,19 @@ mapfile -t NEEDED < \
 	<(sed -n -e 's/^[[:space:]]*\(make\)\?depends\(_x86_64\)\? = \([[:alnum:][:punct:]]*\)[[:space:]]*$/\3/p' .SRCINFO)
 
 
-if [[ -n "$NEEDED" && "$NEEDED" =~ ^[[:alpha:]]+$ ]]; then
-	mapfile -t PKGDEPS < \
-		<(pacman -T ${NEEDED[@]})
+if [ ${#NEEDED[@]} -eq 0 ]; then
+  echo "No dependencies found."
+else
+  if [[ -n "$NEEDED" && "$NEEDED" =~ ^[[:alpha:]]+$ ]]; then
+    mapfile -t PKGDEPS < <(pacman -T ${NEEDED[@]})
 
-	if [[ $NEEDED == *"rust"* ]] || [[ $NEEDED == *"cargo"* ]]; then
-	pacman -Syu --noconfirm rust
-	rustc --version
-	fi
+    if [[ $NEEDED == *"rust"* ]] || [[ $NEEDED == *"cargo"* ]]; then
+      pacman -Syu --noconfirm rust
+      rustc --version
+    fi
 
-	sudo -H -u builder yay -S ${PKGDEPS[@]} --noconfirm --needed
+    sudo -H -u builder yay -S ${PKGDEPS[@]} --noconfirm --needed
+  fi
 fi
 
 # Make the builder user the owner of these files
