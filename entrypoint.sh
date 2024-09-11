e7c2e2dfce71ef04211d0c617f8b5c6f9df952d0
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2024-09-11 19:44:21 +0700

    remove space

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,17 +17,17 @@ ln -fs /usr/bin/clang++ /usr/bin/g++
 
 if [ -n "$INPUT_CFLAGS" ]; then
     echo "Overriding CFLAGS"
-	export CFLAGS = "$INPUT_CFLAGS"
+	export CFLAGS="$INPUT_CFLAGS"
 fi
 
 if [ -n "$INPUT_CXXFLAGS" ]; then
     echo "Overriding CXXFLAGS"
-	export CXXFLAGS = "$INPUT_CXXFLAGS"
+	export CXXFLAGS="$INPUT_CXXFLAGS"
 fi
 
 if [ -n "$INPUT_LDFLAGS" ]; then
     echo "Overriding LDFLAGS"
-	export LDFLAGS = "$INPUT_LDFLAGS"
+	export LDFLAGS="$INPUT_LDFLAGS"
 fi
 
 #force pod2man
