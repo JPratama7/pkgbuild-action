42ecf078e5310112191a98b088afab431aa6747a
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2024-11-11 18:30:42 +0700

    fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,8 @@ if [[ ${y_val[@]} =~ $INPUT_CLANGED ]]; then
         cp "$CONFIG_PATH/clang/lld.conf" "$DEST_CONFIG_PATH"
         cp "$CONFIG_PATH/clang/rust.llvm.conf" "$DEST_CONFIG_PATH"
     fi
-	if [[${y_val[@]} =~ $INPUT_CLANGEDPOLLY]]; then
+
+	if [[ ${y_val[@]} =~ $INPUT_CLANGEDPOLLY]]; then
 		printf "Enabling Polly for Clang\n"
 		cp "$CONFIG_PATH/clang/polly.clang.conf" "$DEST_CONFIG_PATH"
 	fi 
