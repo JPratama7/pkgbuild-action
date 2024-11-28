377fbd6757a3a015e1a94e07fb8c8f63846ca7a5
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2024-11-28 16:24:08 +0700

    add pkgbuild file

--- entrypoint.sh
+++ entrypoint.sh
@@ -113,7 +113,7 @@ if [ ! -d "$INPUT_PKGDIR" ]; then
 fi
 
 cd "${INPUT_PKGDIR:-.}"
-if grep -q "source" $INPUT_PKGDIR; then
+if grep -q "source" $INPUT_PKGDIR/PKGBUILD; then
 	sudo -H -u builder updpkgsums
 fi
 
