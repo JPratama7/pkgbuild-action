91410c05a8d8be5a10e50bcaf64ddf1a474d7178
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2024-12-27 20:30:32 +0700

    replace maxjobs

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,8 @@ llvm_toolchain=()
 
 pacman -Syu --noconfirm yay wayland-protocols pacman-contrib pipewire wget pkgconf cmake ninja meson
 
+sed -i "s/_max_jobs=\"\"/_max_jobs=\"$MAXJOBS\"/" $CONFIG_PATH/param.conf
+
 if [ -n "$INPUT_CFLAGS" ]; then
 	echo "Append $INPUT_CFLAGS to CFLAGS"
 	sed -i "s/_custom_cflags=\"\"/_custom_cflags=\"$INPUT_CFLAGS\"/" $CONFIG_PATH/param.conf
