e4bf2749fd3350b3f31951a7e042fe01e5cf6553
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2025-04-17 22:44:56 +0700

    add custom package

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,11 @@ if [ -n "$INPUT_CFLAGS" ]; then
 	sed -i "s/_custom_cflags=\"\"/_custom_cflags=\"$INPUT_CFLAGS\"/" $CONFIG_PATH/param.conf
 fi
 
+if [-n "$INPUT_CUSTOMPACKAGES" ]; then
+    echo "Installing $INPUT_CUSTOMPACKAGES"
+    pacman -Sy $INPUT_CUSTOMPACKAGES
+fi
+
 if [ -n "$INPUT_CXXFLAGS" ]; then
 	echo "Append $INPUT_CXXFLAGS to CXXFLAGS"
 	sed -i "s/_custom_cxxflags=\"\"/_custom_cxxflags=\"$INPUT_CXXFLAGS\"/" $CONFIG_PATH/param.conf
