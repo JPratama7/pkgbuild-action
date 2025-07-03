3dba7bd509bf31297108f2143070730da01f563b
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2025-07-03 08:38:11 +0700

    fix directive

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ if [ -n "$INPUT_CHAOTICAUR" ]; then
     pacman-key --lsign-key 3056513887B78AEB
     pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
     pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-    sed -i -e '$a\' -e '$a [chaotic-aur]' -e '$a Include = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
+    sed -i -e '$a [chaotic-aur]' -e '$a Include = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
 fi
 
 
