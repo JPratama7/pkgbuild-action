1333038fbc0f7043f28948a99323801d22e5747f
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2025-12-26 12:34:20 +0700

    fix forget spacing

--- entrypoint.sh
+++ entrypoint.sh
@@ -161,7 +161,7 @@ chmod -R 777 .
 
 BASEDIR="$(pwd)"
 
-if [[ " ${y_val[@]} " =~ " $BUILD_AUR_PACKAGE "]]; then
+if [[ " ${y_val[@]} " =~ " $BUILD_AUR_PACKAGE " ]]; then
     printf "Building aur helper $AUR_HELPER"
     pushd /tmp
     sudo -H -u builder git clone https://aur.archlinux.org/$AUR_HELPER.git
